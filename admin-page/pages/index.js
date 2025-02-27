f683bb6d3e724d69b356da7d2a8a8ae41391b48d
SkripsiJal <106738455+LeonKings@users.noreply.github.com>
2025-02-27 16:04:34 +0700

    1

--- admin-page/pages/index.js
+++ admin-page/pages/index.js
@@ -1,32 +1,5 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="bg-blue-600 w-screen h-screen flex items-center justify-center">
-        <div className="w-full text-center">
-          <button
-            className="bg-white p-2 px-2 rounded-lg"
-            onClick={() => signIn("google")} // Pass provider as a string
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <p>Signed in as {session.user.email}</p>
-      <button
-        className="bg-red-500 text-white p-2 px-4 rounded-lg mt-4"
-        onClick={() => signOut()}
-      >
-        Sign out
-      </button>
-    </div>
-  );
+  return <Layout></Layout>;
 }
